04989176cfbaa4998af0e0a3d02addf1012a18df
SghaierMalek <sghaiermalek01@gmail.com>
2019-12-13 11:31:26 +0100

    newFirst

---
+++ SwaggerOdata/Controllers/NinjaController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwaggerOdata.Controllers
+{
+    public class NinjaController
+    {
+    }
+}
