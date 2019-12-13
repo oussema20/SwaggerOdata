d5462e463d7e1cf688b72157f7642b2f0f16cfdb
SghaierMalek <sghaiermalek01@gmail.com>
2019-12-13 15:16:07 +0100

    Add Generic Repository

---
+++ SwaggerOdata/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SwaggerOdata.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwaggerOdata.Persistence.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private AppDbContext _context;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            var rep = await _context.Set<T>().ToListAsync();
+            return rep;
+        }
+    }
+}
